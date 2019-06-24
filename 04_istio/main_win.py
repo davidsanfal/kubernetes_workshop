ba23f6b8ae43c9aa0e1fbd9d6c0f373548348736
David Sanchez <david.sanchez.falero@gmail.com>
2019-06-24 17:32:42 +0200

    istio example updated

--- 04_istio/main_win.py
+++ 04_istio/main_win.py
@@ -2,7 +2,7 @@ import subprocess
 import requests
 import json
 
-result = subprocess.run(["minikube", "ip"], stdout=subprocess.PIPE)
+result = subprocess.run(["minikube.exe", "ip"], stdout=subprocess.PIPE)
 response = requests.get("http://{}".format(result.stdout.decode('utf-8').rstrip()))
 print(json.dumps(dict(response.headers), indent=4, sort_keys=True))
 print(json.dumps(response.json(), indent=4, sort_keys=True))
