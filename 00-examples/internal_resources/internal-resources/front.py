8b1f5881677ae5495a81235b524abd5c19700184
david <david.sanchez@ericsson.com>
2019-07-02 15:31:09 +0200

    example fixed

--- 00-examples/internal_resources/internal-resources/front.py
+++ 00-examples/internal_resources/internal-resources/front.py
@@ -8,14 +8,10 @@ cpu_request = '{}/api/v1/query?query=container_cpu_user_seconds_total\{pod_name=
 
 @app.route("/")
 def version():
-    r = requests.get(
-        cpu_request.format(
-            os.getenv("PROMETHEUS_API_URL"),
-            os.getenv("POD_NAME"),
-            os.getenv("CONTAINER_NAME")
-        )
-    )
-    return jsonify(r.json().get('data').get('result', [{}])[0])
+    url = os.getenv("PROMETHEUS_API_URL") + '/api/v1/query?query=container_cpu_user_seconds_total{pod_name="' + os.getenv("POD_NAME") + '",container_name="' + os.getenv("CONTAINER_NAME")+'"}'
+    r = requests.get(url)
+    # return jsonify({'url': url, 'response': r.text})
+    return jsonify(r.json().get('data', {}).get('result', [{}])[0])
 
 
 if __name__ == '__main__':
